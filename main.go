720e8a3833d67ab64c0b5608bcf5af81f95ba8ce
don caldwell <doncaldwell@dons-Laptop.local>
2024-07-14 08:23:21 -0400

    add emit argument cleanup

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"math/rand"
 	"time"
@@ -15,32 +14,25 @@ func main() {
 	var format string
 	var slen int
 	var rlen bool
-	var n int
+	var emit bool
+	var n int64
 
+	flag.BoolVar(&emit, "emit", false, "emit instead of return")
 	flag.BoolVar(&rlen, "rlen", true, "random lengtha")
 	flag.StringVar(&dtype, "datatype", "string", "type of data to sort - string, uint64, datetime")
 	flag.StringVar(&format, "format", "RFC3339", "if datatype is datetime, what date format")
 	flag.IntVar(&slen, "len", 32, "max length for random strings")
-	flag.IntVar(&n, "n", 1<<20, "number of random data elements to generate")
+	flag.Int64Var(&n, "n", 1<<20, "number of random data elements to generate")
 	flag.Parse()
 
 	rand.Seed(time.Now().UnixNano())
 
 	if dtype == "string" {
-		ssl := randomstrings(n, slen, rlen)
-		for _, l := range ssl {
-			fmt.Println(l)
-		}
+		randomstrings(n, slen, rlen, emit)
 	} else if dtype == "uint64" {
-		usl := randomuints(n)
-		for _, u := range usl {
-			fmt.Println(u)
-		}
+		randomuints(n, emit)
 	} else if dtype == "datetime" {
-		ssl := randomdates(n, format)
-		for _, l := range ssl {
-			fmt.Println(l)
-		}
+		randomdates(n, format, emit)
 	} else {
 		log.Fatal("datatype may only be string, uint64, or datetime")
 	}
--- randomdata.go
+++ randomdata.go
@@ -13,14 +13,14 @@ var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 // randSeq(n int)
 // generate a random string length n with lower, upper case letters and digits
-func randSeq(n int, rlen bool) string {
-	b := make([]rune, n)
+func randSeq(slen int, rlen bool) string {
+	b := make([]rune, slen)
 	ll := len(letters)
 	for i := range b {
 		b[i] = letters[rand.Intn(ll)]
 	}
 	if rlen == true {
-		rl := rand.Intn(n)
+		rl := rand.Intn(slen)
 		if rl != 0 {
 			b = b[:rl]
 		}
@@ -28,13 +28,19 @@ func randSeq(n int, rlen bool) string {
 	return string(b)
 }
 
-// randomstrings(n int, slen int)
+// randomstrings(n int64, slen int)
 // generate n random strings with length slen
 // return a slice containing the strings
-func randomstrings(n int, slen int, rlen bool) []string {
+func randomstrings(n int64, slen int, rlen bool, emit bool) []string {
 	ssl := make([]string, 0)
-	for _ = range n {
-		ssl = append(ssl, randSeq(slen, rlen))
+	if emit == true {
+		for _ = range n {
+			fmt.Println(randSeq(slen, rlen))
+		}
+	} else {
+		for _ = range n {
+			ssl = append(ssl, randSeq(slen, rlen))
+		}
 	}
 	return ssl
 }
@@ -42,42 +48,69 @@ func randomstrings(n int, slen int, rlen bool) []string {
 // randomints(ņ int)
 // generate n random int64 values
 // return a slice containing the int64 values
-func randomuints(n int) []uint64 {
+func randomuints(n int64, emit bool) []uint64 {
 	usl := make([]uint64, 0)
-	for _ = range n {
-		fmt.Println(rand.Uint64())
-		usl = append(usl, rand.Uint64())
+	if emit == true {
+		for _ = range n {
+			fmt.Println(rand.Uint64())
+		}
+	} else {
+		for _ = range n {
+			usl = append(usl, rand.Uint64())
+		}
 	}
 	return usl
 }
 
-// randomdates(n int, format string)
+// randomdates(n int64, format string)
 // generate n random dates with format
 // return a slice containing the random date strings
-func randomdates(n int, format string) []string {
+func randomdates(n int64, format string, emit bool) []string {
 	now := time.Now().Unix()
 	var mod = int64(now)
 	var s string
 	dsl := make([]string, 0)
-	for _ = range n {
-		ri := rand.Int63() % mod
-		tm := time.Unix(int64(ri), int64(0))
+	if emit == true {
+		for _ = range n {
+			ri := rand.Int63() % mod
+			tm := time.Unix(int64(ri), int64(0))
 
-		switch format {
-		case "DateTime":
-			s = fmt.Sprint(tm.Format(time.DateTime))
-		case "Layout":
-			s = fmt.Sprint(tm.Format(time.Layout))
-		case "RubyDate":
-			s = fmt.Sprint(tm.Format(time.RubyDate))
-		case "UnixDate":
-			s = fmt.Sprint(tm.Format(time.UnixDate))
-		case "RFC3339":
-			s = fmt.Sprint(tm.Format(time.RFC3339))
-		default:
-			s = fmt.Sprint(tm)
+			switch format {
+			case "DateTime":
+				fmt.Println(tm.Format(time.DateTime))
+			case "Layout":
+				fmt.Println(tm.Format(time.Layout))
+			case "RubyDate":
+				fmt.Println(tm.Format(time.RubyDate))
+			case "UnixDate":
+				fmt.Println(tm.Format(time.UnixDate))
+			case "RFC3339":
+				fmt.Println(tm.Format(time.RFC3339))
+			default:
+				fmt.Println(tm)
+			}
 		}
-		dsl = append(dsl, s)
+	} else {
+		for _ = range n {
+			ri := rand.Int63() % mod
+			tm := time.Unix(int64(ri), int64(0))
+
+			switch format {
+			case "DateTime":
+				s = fmt.Sprint(tm.Format(time.DateTime))
+			case "Layout":
+				s = fmt.Sprint(tm.Format(time.Layout))
+			case "RubyDate":
+				s = fmt.Sprint(tm.Format(time.RubyDate))
+			case "UnixDate":
+				s = fmt.Sprint(tm.Format(time.UnixDate))
+			case "RFC3339":
+				s = fmt.Sprint(tm.Format(time.RFC3339))
+			default:
+				s = fmt.Sprint(tm)
+			}
+			dsl = append(dsl, s)
+		}
 	}
 	return dsl
 }
